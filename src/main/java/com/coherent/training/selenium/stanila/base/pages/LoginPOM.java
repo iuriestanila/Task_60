f8132ee9af53fb0ed081398ca073b044edf32469
IurieStanila <stanilaiurie@gmail.com>
2022-11-25 10:36:47 +0200

    Fixed the locator issue

--- src/main/java/com/coherent/training/selenium/stanila/base/pages/LoginPOM.java
+++ src/main/java/com/coherent/training/selenium/stanila/base/pages/LoginPOM.java
@@ -1,7 +1,6 @@
 package com.coherent.training.selenium.stanila.base.pages;
 
 import com.coherent.training.selenium.stanila.base.BasePOM;
-import com.coherent.training.selenium.stanila.base.DriverFactory;
 import lombok.SneakyThrows;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,11 +9,11 @@ import org.openqa.selenium.support.FindBy;
 public class LoginPOM extends BasePOM {
     @FindBy(xpath = "//button[starts-with(@class,'Button2')]")
     private WebElement loginFirst;
-    @FindBy (xpath ="//input[@id='passp-field-login']")
+    @FindBy(id = "passp-field-login")
     private WebElement username;
-    @FindBy (xpath = "//button[@id='passp:sign-in']")
+    @FindBy (id= "passp:sign-in")
     private WebElement login;
-    @FindBy(xpath = "//input[@id='passp-field-passwd']")
+    @FindBy(id = "passp-field-passwd")
     private WebElement password;
     @FindBy(xpath = "//div[@data-key='view=react-main-buttons']//div//a[@role='button']")
     private WebElement compose;
--- src/main/java/com/coherent/training/selenium/stanila/base/pages/LogoutPOM.java
+++ src/main/java/com/coherent/training/selenium/stanila/base/pages/LogoutPOM.java
@@ -9,11 +9,11 @@ import org.openqa.selenium.support.FindBy;
 public class LogoutPOM extends BasePOM {
     @FindBy(xpath = "//button[starts-with(@class,'Button2')]")
     private WebElement loginFirst;
-    @FindBy(xpath = "//input[@id='passp-field-login']")
+    @FindBy(id = "passp-field-login")
     private WebElement username;
-    @FindBy(xpath = "//button[@id='passp:sign-in']")
+    @FindBy (id= "passp:sign-in")
     private WebElement login;
-    @FindBy(xpath = "//input[@id='passp-field-passwd']")
+    @FindBy(id = "passp-field-passwd")
     private WebElement password;
     @FindBy(xpath = "//a[@role='button']//img")
     private WebElement image;
